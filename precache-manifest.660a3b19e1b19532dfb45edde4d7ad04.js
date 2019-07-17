63aea10739fe54449e19b6811bbd2d01bde1a315
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-17 15:23:46 +0500

    Updates

---
+++ precache-manifest.660a3b19e1b19532dfb45edde4d7ad04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67606818843dca48fe6765817d18c095",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "88d7a7e8877dcfef5e5b",
+    "url": "/todo-app/static/css/main.89c0d08d.chunk.css"
+  },
+  {
+    "revision": "81c7d0dff5f6ba0b2a4f",
+    "url": "/todo-app/static/js/2.c85ae1d8.chunk.js"
+  },
+  {
+    "revision": "88d7a7e8877dcfef5e5b",
+    "url": "/todo-app/static/js/main.b10e5515.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
